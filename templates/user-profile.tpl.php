e7f149e0b2cc9441f06915ea45113e622c3b65bf
Devon Peel <dp3athome@yahoo.com>
2015-12-16 10:35:58 -0600

    init commit

---
+++ templates/user-profile.tpl.php
@@ -0,0 +1,20 @@
+<div class="profile">
+  <style type="text/css">
+    .userpic {
+      float: right;
+      right: 5px;
+      top: 0px;
+      padding: 5px 5px 5px 5px;
+    }
+  </style>
+
+  <div class="profile"<?php print render($attributes); ?>>
+     <?php print render($user_profile); ?>
+     <?php print render($account); ?>
+
+  </div>
+
+
+
+</div>
+
